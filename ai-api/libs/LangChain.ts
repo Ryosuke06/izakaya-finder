f52aca706e23e58f0b6783ec8243a2b17d6ca9e5
Ryosuke <rykmryoke0406@gmail.com>
2026-02-02 00:26:08 +0900

    google-genaiを追加

---
+++ ai-api/libs/LangChain.ts
@@ -0,0 +1,10 @@
+import { Annotation, MessagesAnnotation } from "@langchain/langgraph"
+import OpenAI from "openai"
+
+const GOOGLE_API_KEY = process.env.GOOGLE_MAPS_API_KEY
+const Gimini_API_KEY = process.env.izakaya_found_gimini_api
+
+async function llmchain_invoke (comment: string) {
+  OpenAIApiKey: Gimini_API_KEY
+}
+
